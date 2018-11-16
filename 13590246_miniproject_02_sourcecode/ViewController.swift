3e16d52d91075c8cf6289c68a2bf343bc36856cf
Shinchan Wassup <chinjung@chinjungs-macbook-pro.local>
2018-11-17 06:13:28 +0700

    Initial Commit

---
+++ 13590246_miniproject_02_sourcecode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  13590246_miniproject_02_sourcecode
+//
+//  Created by SHINCHAN on 17/11/2561 BE.
+//  Copyright © 2561 Kukeat Chaiwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
